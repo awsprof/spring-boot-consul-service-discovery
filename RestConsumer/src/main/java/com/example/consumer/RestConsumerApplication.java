f8864cfcd2503ab66f8775135ec35708ea6f3114
awsprof <rsendhil@srivittal.com>
2023-03-05 13:49:40 +0000

    Fixed Consul discovery and config

--- RestConsumer/src/main/java/com/example/consumer/RestConsumerApplication.java
+++ RestConsumer/src/main/java/com/example/consumer/RestConsumerApplication.java
@@ -1,11 +1,14 @@
 package com.example.consumer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties
 public class RestConsumerApplication {
 
     public static void main(String[] args) {
